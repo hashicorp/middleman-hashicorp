0b28ad90723a495b43818213bfe7c43737e469df
Seth Vargo <sethvargo@gmail.com>
2015-10-25 23:14:14 -0400

    Add BC methods for Bintray

--- lib/middleman-hashicorp/extension.rb
+++ lib/middleman-hashicorp/extension.rb
@@ -223,16 +223,16 @@ class Middleman::HashiCorpExtension < ::Middleman::Extension
   # @return [Hash]
   #
   def real_product_versions
-    # if options.bintray_enabled
-    #   BintrayAPI.new(
-    #     repo:     options.bintray_repo,
-    #     user:     options.bintray_user,
-    #     key:      options.bintray_key,
-    #     filter:   options.bintray_exclude_proc,
-    #     prefixed: options.bintray_prefixed,
-    #   ).downloads_for_version(options.version)
-    # else
+    if options.bintray_enabled
+      BintrayAPI.new(
+        repo:     options.bintray_repo,
+        user:     options.bintray_user,
+        key:      options.bintray_key,
+        filter:   options.bintray_exclude_proc,
+        prefixed: options.bintray_prefixed,
+      ).downloads_for_version(options.version)
+    else
       Middleman::HashiCorp::Releases.fetch(options.name, options.version)
-    # end
+    end
   end
 end
